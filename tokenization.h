7aa1f08b6574162d74b762c8686b889ab41d1d16
Saimaneesh <yeturu.saimaneesh@gmail.com>
2023-12-01 11:16:01 +0100

    Created template file for tokenization

---
+++ tokenization.h
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1024
+#define INPUT_SIZE 1024
+
+void tokenize_input(char ***tokens, int* n, char* file, char* delimiters){
+
+    int size = INPUT_SIZE+1;
+
+    char buffer[BUFFER_SIZE];
+    const char *delimiter_characters = delimiters;
+
+    FILE *fp = fopen(file, "r");
+    if (!fp){
+        fprintf(stderr, "cannot open %s: %s\n", "test.txt", strerror(errno));
+    }
+
+    char *token;
+
+    int i = 0;
+    while (fgets(buffer, BUFFER_SIZE, fp)) {
+        token = strtok(buffer, delimiter_characters);
+
+        while (token != NULL){ 
+
+            if (i >= size){ // dynamically change size of array
+                size *= 2;
+                char** temp = (char**) realloc(*tokens, (size+1) * sizeof(char*));
+                if (temp == NULL){
+                    printf("Error in allocation");
+                }
+                tokens = &temp;
+            }
+
+
+            (*tokens)[i] = (char*) malloc((strlen(token)+1) * sizeof(char));
+            strcpy((*tokens)[i], token);
+            token = strtok(NULL, delimiter_characters);
+            i++;
+        }            
+    }
+
+    *n = i;
+    
+    fclose(fp);
+}
+
+void free_tokens(char** tokens, int n){
+    for (int i = 0; i < n; i++){
+        free(tokens[i]);
+    }
+    free(tokens);
+}
