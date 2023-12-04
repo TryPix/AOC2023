7beef07fc4afed8859794142fd4ea55175316eaa
Saimaneesh <yeturu.saimaneesh@gmail.com>
2023-12-04 19:51:34 +0100

    clean up

--- day4.c
+++ day4.c
@@ -17,33 +17,29 @@ int main() {
     int sum2 = 0;
 
     int scratch[n];
-    for (int i = 0; i< n; i++){
-        scratch[i] = 1;
-    }
 
-    printf("\n\n");
+    int pattern = 1;
+    memset_pattern4(scratch, &pattern, sizeof(scratch));
+
+    for (int i = 0; i < n; i++) scratch[i] = 1;
 
     for (int i = 0; i < n; i++){
         char* str = tokens[i];
         char* num = strtok(str, ":");
-        int win[256];
-        int all[256];
+        int win[10];
+        int all[25];
         int j = -1;
         int k = 0;
         int ch = 1;
         while (num != NULL){
             if (strstr(num, "|")){
                 ch = 0;
-            } else {
-                if (ch){
-                    win[j] = strtol(num, NULL, 10);
-                    j++;
-                } else{
-                    all[k] = strtol(num, NULL, 10);
-                    k++;
-                }
+            } else if (ch) {
+                win[j] = strtol(num, NULL, 10);
+                j++;
+            } else { all[k] = strtol(num, NULL, 10);
+                k++;
             }
-
             num = strtok(NULL, " ");
         }
 
@@ -52,10 +48,11 @@ int main() {
 
         for (int t = 0; t < j; t++){
             int v = win[t];
+
            
             for (int l = 0; l < k; l++){
                 if (v == all[l]){
-                    if (factor==0) {factor = 1; nCards++; break;}
+                    if (factor == 0) {factor = 1; nCards++; break;}
                     factor *= 2;
                     nCards++;
                     break;
@@ -73,15 +70,10 @@ int main() {
 
     for (int i = 0; i < n; i++){
         sum2 += scratch[i];
-        // printf("hey %d ", scratch[i]);
     }
 
-
-
-
-
-    printf("%d\n", sum);
-    printf("%d\n", sum2);
+    printf("Part 1: %d\n", sum);
+    printf("Part 2: %d\n", sum2);
 
 
     free_tokens(tokens, n);
