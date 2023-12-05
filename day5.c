c687131869343a77a4ab695c0b823e709fb77528
Saimaneesh <yeturu.saimaneesh@gmail.com>
2023-12-05 21:06:30 +0100

    Clean-up, multithreading (4x speedup)

--- day5.c
+++ day5.c
@@ -4,10 +4,25 @@
 #include <string.h>
 #include <ctype.h> 
 #include <limits.h>
+#include <pthread.h>
 #include "tokenization.h"
 #include "utilities.h"
 
-long applytransform(long seed, long size, long transform[size][3]){
+int n;
+long transforms[7][64][3];
+long sizes[7];
+long seeds[32];
+int nseeds;
+
+typedef struct
+{
+    long start;
+    long length;
+
+    long min_location;
+} Range;
+
+long map1stage(long seed, long size, long transform[size][3]){
     for (long i = 0; i < size; i++){
         long dest_r = transform[i][0];
         long source_r = transform[i][1];
@@ -20,36 +35,49 @@ long applytransform(long seed, long size, long transform[size][3]){
     return seed;
 }
 
+void map(void *arg){
 
-int main() {
+    Range* r = (Range*) arg;
 
-    char **tokens;
-    int n;
+    long best = LONG_MAX;
+    long start = r->start;
+    long length = r->length;
 
-    tokenize_input(&tokens, &n, "inputs/day5.txt", "\n ");
+    for (long i = start; i < start+length; i++){
+        long val = i;
+        for (long j = 0; j < 7; j++){
+            val = map1stage(val, sizes[j], transforms[j]);
+        }
+        best = val < best ? val : best;
+    }
 
-    long seeds[32];
-    memset(seeds, 0, 32);
+    r->min_location = best;
+}
 
-    long transforms[7][64][3];
-    memset(transforms, 0, sizeof(transforms));
+void* map_threads(void *arg){
+    Range* r = (Range*) arg;
+    map(r);
+    pthread_exit(NULL);
+}
+
+void parse(char*** tokens){
 
     long index = 1;
-    while (isdigit(tokens[index][0])){
-        seeds[index-1] = strtol(tokens[index], NULL, 10);
+    while (isdigit((*tokens)[index][0])){
+        seeds[index-1] = strtol((*tokens)[index], NULL, 10);
         index++;
     }
-    long nseeds = index-1;
+    nseeds = index-1;
 
-    long transform_index = 0;
-    long curr_index = 0;
+    int transform_index = 0;
+    int curr_index = 0;
 
-    long sizes[7];
+
     memset(sizes, 0, 7);
     long rot = 0;
 
     for (long i = index+2; i < n; i++){
-        if (!isdigit(tokens[i][0])){
+        if (!isdigit((*tokens)[i][0])){
             sizes[transform_index] = curr_index;
             transform_index++;
             i++;
@@ -57,7 +85,7 @@ int main() {
             rot = 0;
             continue;
         }
-        transforms[transform_index][curr_index][rot] = strtol(tokens[i], NULL, 10);
+        transforms[transform_index][curr_index][rot] = strtol((*tokens)[i], NULL, 10);
         rot++;
         if(rot==3){
             curr_index++;
@@ -65,42 +93,66 @@ int main() {
         }
     }
 
-    free_tokens(tokens, n);
+    free_tokens((*tokens), n);
 
     sizes[6] = curr_index;
 
+}
+
+void part1(){
     long best = LONG_MAX;
 
-    for (long seed = 0; seed < nseeds; seed++){
-        long val = seeds[seed];
-        for (long i = 0; i < 7; i++){
-            val = applytransform(val, sizes[i], transforms[i]);
-        }
-        if (val < best){
-            best = val;
-        }
+    for (int i = 0; i < nseeds; i++){
+        Range r;
+        r.length = 1;
+        r.start = seeds[i];
+        map(&r);
+        best = r.min_location < best ? r.min_location : best;
     }
 
     printf("Part 1: %ld\n", best);
+}
 
-    best = LONG_MAX;
+void part2(){
 
-    for (long i = 0; i < nseeds; i += 2){
-        long start = seeds[i];
-        long length = seeds[i+1];
-        for (long seed = start; seed < start + length-1; seed++){
-            long val = seed;
-            for (long i = 0; i < 7; i++){
-                val = applytransform(val, sizes[i], transforms[i]);
-            }
-            if (val < best){
-                best = val;
-            }
-        }
-
-    }
-    printf("Part 2: %ld\n", best);
+    long best = LONG_MAX;
+    Range ranges[nseeds];
     
+    for (int i = 0; i < nseeds; i+=2){
+        Range r;
+        r.start = seeds[i];
+        r.length = seeds[i+1];
+        r.min_location = LONG_MAX;
+        ranges[i / 2] = r;
+    }
+
+    int k = nseeds/2;
+    pthread_t threads[k];
+
+    for (int i = 0; i < k; i++){
+        int rc = pthread_create(&threads[i], NULL, map_threads, (void*) &ranges[i]);
+        if (rc) printf("Error: Unable to create thread #%d\n", i);
+    }
+
+    for (int i = 0; i < k; i++) {
+        pthread_join(threads[i], NULL);
+        best = ranges[i].min_location < best ? ranges[i].min_location : best;
+    }
+
+    printf("Part 2: %ld\n", best);
+
+}
+
+int main() {
+
+    char **tokens;
+
+    tokenize_input(&tokens, &n, "inputs/day5.txt", "\n ");
+
+    parse(&tokens);
+
+    part1();
+    part2();
 
     return 0;
 }
